f0ee438506b94347bea3039686cabb77e31a0d76
tro9lh <troydima@yandex.ru>
2018-09-30 15:59:12 +0300

    add Boston.py

--- Boston.py
+++ Boston.py
@@ -3,7 +3,10 @@ import json
 
 drug_name = str(input("Введите название препарата (Например Placebo): "))
 phase = str(input("Введите фазу испытаний в формате I: "))
-params = {'arms.interventions.intervention_type': 'Drug', 'arms.interventions.intervention_name': drug_name, 'phase.phase': phase}
+params = {
+            'arms.interventions.intervention_type': 'Drug',
+            'arms.interventions.intervention_name': drug_name,
+            'phase.phase': phase}
 url = 'https://clinicaltrialsapi.cancer.gov/v1/clinical-trials'
 r = requests.get(url, params=params)
 json_data = json.loads(r.text)
